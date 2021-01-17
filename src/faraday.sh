b2b4d34acb0bd157f44c2f8d8560e2555ad70de4
Federico Lopez L <flopezluskenberg@gmail.com>
2021-01-17 13:39:48 -0300

    Updated cli commands

--- src/faraday.sh
+++ src/faraday.sh
@@ -7,14 +7,14 @@ faraday::authenticate() {
   local FARADAY_USERNAME=$2
   local FARADAY_PASSWORD=$3
 
-  echo "N" | faraday-cli auth --url $FARADAY_HOST --user $FARADAY_USERNAME --password $FARADAY_PASSWORD
+  echo "N" | faraday-cli auth -f $FARADAY_HOST -u $FARADAY_USERNAME -p $FARADAY_PASSWORD
 }
 
 faraday::create_workspace() {
   local FARADAY_WORKSPACE=$1
 
   echo "Creating workspace $FARADAY_WORKSPACE if it doesn't exists"
-  faraday-cli workspace -a create -n $FARADAY_WORKSPACE > /dev/null
+  faraday-cli create_ws $FARADAY_WORKSPACE
   echo "Workspace $FARADAY_WORKSPACE created/selected successfully"
 }
 
@@ -23,7 +23,7 @@ faraday::upload_report() {
   local FARADAY_REPORT_FILENAME=$2
 
   echo "Uploading report to $FARADAY_WORKSPACE"
-  faraday-cli report -ws $FARADAY_WORKSPACE $FARADAY_REPORT_FILENAME
+  faraday-cli process_report -w $FARADAY_WORKSPACE $FARADAY_REPORT_FILENAME
 
   echo "Faraday import finished successfully"
 }
